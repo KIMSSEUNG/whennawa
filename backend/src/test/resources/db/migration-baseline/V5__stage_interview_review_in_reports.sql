6d35ddaeaab24482f5146400699c3a08c1e938bc
KIMSSEUNG <99792478+KIMSSEUNG@users.noreply.github.com>
2026-03-18 21:38:11 +0900

    update : v5 ddl sql v1으로 통합

--- backend/src/test/resources/db/migration-baseline/V5__stage_interview_review_in_reports.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE recruitment_report
-  ADD COLUMN interview_review_content VARCHAR(2000) NULL,
-  ADD COLUMN interview_difficulty VARCHAR(16) NULL;
-
-ALTER TABLE rolling_report
-  ADD COLUMN interview_review_content VARCHAR(2000) NULL,
-  ADD COLUMN interview_difficulty VARCHAR(16) NULL;
